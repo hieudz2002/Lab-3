4f1344260c791f2d5bf8bed97ea4605cf23ad677
hieudz2002 <109191912+hieudz2002@users.noreply.github.com>
2023-11-10 21:12:13 +0700

    first commit

---
+++ FirebaseAuth-main/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {LoginScreen,SignupScreen} from './screens';
+const App = () => {
+  return (
+    <SafeAreaProvider>
+      <LoginScreen />
+    </SafeAreaProvider>
+  );
+};
+export default App;
